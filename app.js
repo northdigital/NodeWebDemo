c63304f9207103fb345862665deeba3d105b3eed
northdigital <10321991+northdigital@users.noreply.github.com>
2020-09-22 19:46:39 +0300

    commit

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const expess = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 
 const app = expess();
-app.engine("handlebars", handlebars());
-app.set("view engine", "handlebars");
+
+app.engine("hbs", handlebars({
+  defaultLayout: "",
+  //layoutsDir: path.join(__dirname, "views/layouts"),
+  //partialsDir: path.join(__dirname, "views/partials"),
+  extname: "hbs"
+}));
+
+app.set("view engine", "hbs");
+
+// routing
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// end routing
 
 app.listen(7171, (req, res) => {
   console.log("server started at port 7171, http://localhost:7171")
