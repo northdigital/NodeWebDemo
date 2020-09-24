df795922a5b9a7de365e2a15e531d822973f0a9e
northdigital <10321991+northdigital@users.noreply.github.com>
2020-09-24 23:42:22 +0300

    commit

---
+++ routes/handlers.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home Page", name: "Panagiotis", displayName: true });
+});
+
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About Page", copywrite: "(c) Panagiotis" });
+});
+
+router.get("/helpers/each", (req, res) => {
+  res.render("each", {
+    title: "Each Helper Page", teams: [
+      "PAOK",
+      "ARIS",
+      "AEK",
+      "REAL"
+    ],
+    user: { firstName: "PAKIS", lastName: "PAPAKIS" }
+  });
+});
+
+router.get("/helpers/each2", (req, res) => {
+  res.render("each2", {
+    title: "Each2 Helper Page",
+    warehouse: [
+      { room: [{ product: "computers", price: 100 }, { product: "hifi", price: 70 }] },
+      { room: [{ product: "cars", price: 1000 }, { product: "bikes", price: 170 }] },
+      { room: [{ product: "tomatoes", price: 20 }, { product: "potatoes", price: 10 }] }
+    ]
+  });
+});
+
+router.get("/helpers/lookup", (req, res) => {
+  res.render("lookup", {
+    title: "Lookup Page",
+    data: { subdata: { thevalue: "some value" } },
+    teams: ["PAOK", "ARIS", "AEK", "REAL"]
+  });
+});
+
+router.get("/helpers/customhelper", (req, res) => {
+  res.render("customhelper", {
+    title: "Custom Helper Page",
+    data: { subdata: { thevalue: "some value" } },
+    teams: ["PAOK", "ARIS", "AEK", "REAL"]
+  });
+});
+
+router.get("/knockout", (req, res) => {
+  res.render("knockout", {
+    title: "knockout demo",
+    someValue: "this is a handlebar value"
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const expess = require("express");
+const express = require("express");
 const expHandlebars = require("express-handlebars");
 const path = require("path");
+const routes = require("./routes/handlers");
 
-const app = expess();
+const PORT = process.env.PORT || 7171;
+
+const app = express();
 const expHbs = expHandlebars.create({
   defaultLayout: "main",
   layoutsDir: path.join(__dirname, "views/layouts"),
@@ -40,66 +43,10 @@ expHbs.handlebars.registerHelper('knockoutjs', () => {
 app.engine("hbs", expHbs.engine);
 
 app.set("view engine", "hbs");
-app.use('/modules', expess.static(path.join(__dirname, 'node_modules')));
+app.use('/modules', express.static(path.join(__dirname, 'node_modules')));
 
-// routing
+app.use("/", routes);
 
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home Page", name: "Panagiotis", displayName: true });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About Page", copywrite: "(c) Panagiotis" });
-});
-
-app.get("/helpers/each", (req, res) => {
-  res.render("each", {
-    title: "Each Helper Page", teams: [
-      "PAOK",
-      "ARIS",
-      "AEK",
-      "REAL"
-    ],
-    user: { firstName: "PAKIS", lastName: "PAPAKIS" }
-  });
-});
-
-app.get("/helpers/each2", (req, res) => {
-  res.render("each2", {
-    title: "Each2 Helper Page",
-    warehouse: [
-      { room: [{ product: "computers", price: 100 }, { product: "hifi", price: 70 }] },
-      { room: [{ product: "cars", price: 1000 }, { product: "bikes", price: 170 }] },
-      { room: [{ product: "tomatoes", price: 20 }, { product: "potatoes", price: 10 }] }
-    ]
-  });
-});
-
-app.get("/helpers/lookup", (req, res) => {
-  res.render("lookup", {
-    title: "Lookup Page",
-    data: { subdata: { thevalue: "some value" } },
-    teams: ["PAOK", "ARIS", "AEK", "REAL"]
-  });
-});
-
-app.get("/helpers/customhelper", (req, res) => {
-  res.render("customhelper", {
-    title: "Custom Helper Page",
-    data: { subdata: { thevalue: "some value" } },
-    teams: ["PAOK", "ARIS", "AEK", "REAL"]
-  });
-});
-
-app.get("/knockout", (req, res) => {
-  res.render("knockout", {
-    title: "knockout demo",
-    someValue: "this is a handlebar value"
-  });
-});
-
-// end routing
-
-app.listen(7171, (req, res) => {
-  console.log("server started at port 7171, http://localhost:7171")
+app.listen(PORT, (req, res) => {
+  console.log(`server started at port ${PORT}, http://localhost:${PORT}`)
 });
